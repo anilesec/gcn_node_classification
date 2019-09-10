ba4730b85b50a97a98e15fd1731ef8336d4a69f6
Anilkumar Swamy <aswamy@corbier.ad.europe.naverlabs.com>
2019-09-10 12:28:15 +0200

    gcn layer update

--- layers.py
+++ layers.py
@@ -1,7 +1,6 @@
 import math
-
 import torch
-
+import torch.nn as nn
 from torch.nn.parameter import Parameter
 from torch.nn.modules.module import Module
 
@@ -11,42 +10,61 @@ class GraphConvolution(Module):
     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
     """
 
-    def __init__(self, in_features, out_features, bias=True):
+    def __init__(self, in_features, out_features):
         super(GraphConvolution, self).__init__()
-        self.in_features = in_features
-        self.out_features = out_features
-        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
-        if bias:
-            self.bias = Parameter(torch.FloatTensor(out_features))
-        else:
-            self.register_parameter('bias', None)
-        self.reset_parameters()
 
-    def reset_parameters(self):
-        stdv = 1. / math.sqrt(self.weight.size(1))
-        self.weight.data.uniform_(-stdv, stdv)
-        if self.bias is not None:
-            self.bias.data.uniform_(-stdv, stdv)
+        self.linear = nn.Linear(in_features, out_features)
 
-    def forward(self, in_put, adj):
-        # # input('enter for input')
-        # print(in_put, in_put.shape)
-        # input('enter for weight')
-        # print(self.weight, self.weight.shape)
-        support = torch.mm(in_put, self.weight)
-        # input('enter for support')
-        # print(support, support.shape) 
-        output = torch.mm(adj, support)
-        # input('enter for output')
-        # print(output, output.shape)
-        # print(support.sum())
-        # input('continue')
-        if self.bias is not None:
-            return output + self.bias
-        else:
-            return output
+    def forward(self, x, adj):
+        y = x
+        y = torch.mm(adj, self.linear(y))
 
-    def __repr__(self):
-        return self.__class__.__name__ + ' (' \
-               + str(self.in_features) + ' -> ' \
-               + str(self.out_features) + ')'
+        return y
+
+
+# class GraphConvolution(Module):
+#     """
+#     Simple GCN layer, similar to https://arxiv.org/abs/1609.02907
+#     """
+
+#     def __init__(self, in_features, out_features, bias=True):
+#         super(GraphConvolution, self).__init__()
+#         self.in_features = in_features
+#         self.out_features = out_features
+#         self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+#         if bias:
+#             self.bias = Parameter(torch.FloatTensor(out_features))
+#         else:
+#             self.register_parameter('bias', None)
+#         self.reset_parameters()
+
+#     def reset_parameters(self):
+#         stdv = 1. / math.sqrt(self.weight.size(1))
+#         self.weight.data.uniform_(-stdv, stdv)
+#         if self.bias is not None:
+#             self.bias.data.uniform_(-stdv, stdv)
+
+#     def forward(self, in_put, adj):
+#         # # input('enter for input')
+#         # print(in_put, in_put.shape)
+#         # input('enter for weight')
+#         # print(self.weight, self.weight.shape)
+#         support = torch.mm(in_put, self.weight)
+#         # input('enter for support')
+#         # print(support, support.shape) 
+#         output = torch.mm(adj, support)
+#         # input('enter for output')
+#         # print(output, output.shape)
+#         # print(support.sum())
+#         # input('continue')
+#         if self.bias is not None:
+#             return output + self.bias
+#         else:
+#             return output
+
+#     def __repr__(self):
+#         return self.__class__.__name__ + ' (' \
+#                + str(self.in_features) + ' -> ' \
+#                + str(self.out_features) + ')'
+
+
